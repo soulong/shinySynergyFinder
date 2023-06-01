fb32dd79f1877fabf4f609ea6ef5703ebaf3dbdc
Hao He <haohe90@gmail.com>
2023-06-01 13:04:05 +0800

    shiny split

--- synergyfinder.R
+++ synergyfinder.R
@@ -5,7 +5,6 @@ library(synergyfinder)
 library(tidyverse)
 library(patchwork)
 library(magrittr)
-library(shiny)
 
 
 ## func
@@ -154,58 +153,6 @@ if(F) {
   
 }
 
-
-
-## use shiny
-if(T) {
-  server <- function(input, output) {
-    
-    data <- eventReactive(input$submit, {
-      tmpdir <- tempdir()
-      withProgress(
-        read_csv(input$input_file$datapath) %>%
-        run_synergyfinder(type=input$type, save_dir=tmpdir),
-        message='Calculating ...', value=0.4)
-      res_files <- list.files(tmpdir, pattern='(.pdf)|(.xlsx)', full.names=F)
-      # print(res_files)
-      return(list(tmpdir=tmpdir, res_files=res_files))
-    })
-    
-    output$res_show <- renderPrint({
-      data()
-    })
-    
-    output$download <- downloadHandler(
-      filename = 'result.zip',
-      content = function(fname) {
-        setwd(data()$tmpdir)
-        zip(zipfile=fname, files=data()$res_files)
-      },
-      contentType = "application/zip"
-    )
-  }
-  
-  ui <- shinyUI(
-    fluidPage(
-      title='SynergyFinder',
-      titlePanel('SynergyFinder from Bioconductor Version'),
-      sidebarLayout(
-        sidebarPanel(
-          fileInput('input_file', 'Upload formated csv file', accept='.csv'),
-          radioButtons('type', 'Effect type', c('viability','inhibition'), selected='viability', inline=F),
-          actionButton('submit', 'Submit'),
-          downloadButton('download', 'Download', icon=icon("file-download"))
-        ),
-        mainPanel(
-          verbatimTextOutput('res_show')
-        )
-      )
-    )
-  )
-  
-  app <- shinyApp(ui, server)
-  runApp(app, host='0.0.0.0', port=5000L)
-}
 
 
 
